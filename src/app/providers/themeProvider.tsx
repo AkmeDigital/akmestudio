57ca10a3eee8112c573d914df0c90e0bc090a4d1
Adrian T <adrian.thorburn@gmail.com>
2025-05-18 16:38:42 +1200

    scroll fix on home page

--- src/app/providers/themeProvider.tsx
+++ src/app/providers/themeProvider.tsx
@@ -14,6 +14,7 @@ export default function ThemeProvider() {
     let textColor = "#0a0a0a";
     let backgroundColor = "#fff";
     let headerColor = "#fff";
+    let overflow = "visible";
 
     // Update styles based on specific pathnames
     switch (pathname) {
@@ -21,22 +22,24 @@ export default function ThemeProvider() {
         textColor = "#0a0a0a";
         backgroundColor = "#fff";
         headerColor = "#fff";
-        // textColor = "#fff";
-        // backgroundColor = "#0a0a0a";
+        overflow = "visible";
         break;
       case "/work":
         textColor = "#fff";
         backgroundColor = "#0a0a0a";
+        overflow = "visible";
         break;
       case "/":
         textColor = "#fff";
         backgroundColor = "#0a0a0a";
+        overflow = "hidden";
         break;
       default:
         if (pathname.startsWith("/work/")) {
           backgroundColor = "#fff";
           textColor = "#0a0a0a";
           headerColor = "#fff";
+          overflow = "visible";
         }
         break;
     }
@@ -45,6 +48,7 @@ export default function ThemeProvider() {
     root.style.setProperty("--text-color", textColor);
     root.style.setProperty("--background-color", backgroundColor);
     root.style.setProperty("--header-color", headerColor);
+    root.style.setProperty("--overflow", overflow);
   }, [pathname]);
 
   return null; // No UI, just a side effect
